d9bc2450d51fc9c3ff46b9f012f8ef559cb3ecc7
saikaew <nunuza002@hotmail.com>
2016-07-13 09:45:36 +0700

    Start 13 july

---
+++ app/src/main/java/appewtc/masterung/rusrun/MyAlert.java
@@ -0,0 +1,18 @@
+package appewtc.masterung.rusrun;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+/**
+ * Created by Nam on 7/13/2016.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    }
+
+
+}//Main Class
