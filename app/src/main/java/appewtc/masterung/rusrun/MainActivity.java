76feb8a53e352d16830fd85f59deb1497c289d5b
saikaew <nunuza002@hotmail.com>
2016-07-14 10:46:34 +0700

    JSON String

--- app/src/main/java/appewtc/masterung/rusrun/MainActivity.java
+++ app/src/main/java/appewtc/masterung/rusrun/MainActivity.java
@@ -1,12 +1,18 @@
 package appewtc.masterung.rusrun;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private static final String urlLogo = "http://swiftcodingthai.com/rus/image/logo_rus.png";
     private String userString, passwordString;
+    private static final String urlJSON = "http://swiftcodingthai.com/rus/get_user_saikaew.php";
 
 
     @Override
@@ -32,6 +39,56 @@ public class MainActivity extends AppCompatActivity {
         Picasso.with(this).load(urlLogo).into(imageView);
     }   // Main Method
 
+
+    //create Inner Class ต้องการสร้างคลาสภายใน
+
+    private class SynUser extends AsyncTask<Void, Void, String> {
+
+        //Explicit
+        private String myJSONString, myUserString, myPasswordString;
+        private Context context;
+
+        public SynUser(String myJSONString,
+                       String myUserString,
+                       String myPasswordString,
+                       Context context) {
+            this.myJSONString = myJSONString;
+            this.myUserString = myUserString;
+            this.myPasswordString = myPasswordString;
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(myJSONString).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+
+                Log.d("RusV1", "e doIn ==>" + e.toString());
+                return null;
+
+            } //การเชื่อมต่อ
+
+        }//doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("RusV1", "JSON ==>" + s);
+
+        }//onPost
+    } //SynUser Class
+
+
     public void clickSignIn(View view) {
 
         userString = userEditText.getText().toString().trim();
@@ -46,10 +103,12 @@ public class MainActivity extends AppCompatActivity {
                     "Please Fill All EveryBlank ");
         } else {
             //No Space
+            SynUser synUser = new SynUser(urlJSON, userString, passwordString, this);
+            synUser.execute();
 
         } // if
 
-    }//click sign
+    }//click signIn
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
